49efe2d302cd43279d4137ea4266714a46fb8fd3
Roy Ali Hasan <informativepath0987@gmail.comgit config --global user.email informativepath0987@gmail.com>
2021-10-25 13:30:54 +0500

    "I am practice on Vue life Cycle Hooks of components "

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <hook />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import hook from "./components/hook.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    hook,
+  },
+};
 </script>
 
 <style>
@@ -21,8 +24,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+} 
 </style>
---
+++ src/components/hook.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <span>{{mess}}</span>
+    <br>
+    {{ data }}
+    
+  </div>
+</template>
+<script>
+export default {
+  name: "hook",
+  data() {
+    return {
+      mess: "Basic Understanding of Before created and Created",
+      data:'I am created Now'
+      
+    };
+  },
+  beforeCreate() {
+    document.write("This is Before created function:" + this.mess);
+  },
+//   this 
+  Created() {
+    document.write("This is Created function:" + this.mess);
+  },
+};
+</script>
